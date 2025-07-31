65c8c64b33589aee63bea74ff46fec6543bc6297
Tuktin Kostya <kostyatuktin@gmail.com>
2025-08-01 00:53:45 +0300

    add component -  button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+  import Button from "./components/Button.vue";
+
   const id = "green";
 </script>
 
 <template>
   <header :class="`color-${id}`">Лого</header>
+  <Button />
   <main :id>Контент</main>
 </template>
 
---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+    <button class="button">Кнопка</button>
+</template>
+
+<style scoped>
+    .button {
+        border: none;
+        border-radius: 10px;
+        background: var(--gradient-btn);
+        padding: 14px;
+        font-family: var(--font);
+        font-size: 20px;
+        font-weight: 600;
+        color: var(--color-primary-inverted);
+        cursor: pointer;
+    }
+
+    .button:hover{
+        background: var(--gradient-btn-invert);
+    }
+</style>
