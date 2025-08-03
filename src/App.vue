2ed9b3579d2413f25f9e20074d8d9c02ac87880e
Tuktin Kostya <kostyatuktin@gmail.com>
2025-08-03 14:21:33 +0300

    added error handler

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@
   import { ref, computed } from 'vue';
   import Statistic from "./components/Statistic.vue";
   import SelectCity from "./components/SelectCity.vue";
+  import Error from './components/Error.vue';
 
   const API_ENDPOINT = "http://api.weatherapi.com/v1"; //домен источника информации о погоде
   
   //реактивный объект статистики
   const statistic = ref();
+  //реактивное состояние для случая, когда сапрос к WeatherAPI вернулся с ошибкой
+  const error = ref();
 
+  const errorMap = new Map([ [1006, "Не удалось найти город"] ]); // словарь возможных ошибок на русском
+  
   const statModifed = computed(() => {
       if(!statistic.value) {
         return [];
@@ -29,8 +34,12 @@
       ]
   });
 
-  //нужон обработчик
-  async function getCity(city) { // функция получения названия города из компонента SelectCity
+  const errorModified = computed(() => { // по коду ошибки определяет (как ключ), какое сообщение об ошибке будет отображено
+    return errorMap.get(error.value?.error?.code);
+  });
+
+  // функция получения названия города из компонента SelectCity
+  async function getCity(city) { 
     const params = new URLSearchParams({ //для корректного преобразования списка параметров запроса в URL 
       q: city,
       lang: "ru",
@@ -38,6 +47,12 @@
       days: 7,
     });
     const res = await fetch(`${API_ENDPOINT}/forecast.json?${params.toString()}`); //отправка запроса по указанному URL и получение ответа в виде JSON (await приобстанавливает функцию пока не придет ответ от сервера)
+    if(res.status != 200){
+      error.value = await res.json(); // в случае ошибки сохраняем полученную от сервера информацию
+      statistic.value = null;
+      return;
+    } 
+    error.value = null;
     statistic.value = await res.json(); // преобразование строки в объект (так как строка JSON очень большая нужно дождаться завершения преобразования)
   }
 
@@ -45,6 +60,8 @@
 
 <template>
   <main class="main">
+    <Error :error="errorModified"/>
+
     <!-- Мультиплексирование статистики, данные которой динамичкси (передаём как html-аргкмент) могут пересчитываться и всавляться в шаблон -->
     <Statistic v-for="item in statModifed" v-bind="item" :key="item.label"/>
 
---
+++ src/components/Error.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps({
+        error: String,
+    });
+</script>
+
+<template>
+    <div v-if="props.error" class="error">
+        {{ props.error }}
+    </div>
+</template>
+
+<style scoped>
+    .error{
+        font-size: 18px;
+        padding: 15px;
+        text-align: center;
+        background-color: var(--color-bg-info);
+        border-radius: 0px 0px 25px 25px;
+        margin-bottom: 60px;
+        margin-top: -60px;
+    }
+</style>
