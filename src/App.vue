97be5a33d0ba0b5dc6c1701375285a3d8b22e6db
Tuktin Kostya <kostyatuktin@gmail.com>
2025-08-04 18:27:01 +0300

    very unstable zone with city property city

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,19 @@
 <!-- eslint-disable no-undef -->
 <script setup>
-  import { ref, computed } from 'vue';
-  import Statistic from "./components/Statistic.vue";
-  import SelectCity from "./components/SelectCity.vue";
-  import Error from './components/Error.vue';
-  import DayCard from './components/DayCard.vue';
+  import { ref } from 'vue';
+  import PanelRight from "./components/PanelRight.vue";
 
   const API_ENDPOINT = "https://api.weatherapi.com/v1"; //домен источника информации о погоде
   
-  //реактивный объект статистики
-  const statistic = ref();
-  //реактивное состояние для случая, когда сапрос к WeatherAPI вернулся с ошибкой
-  const error = ref();
-  let activeIndex = ref(0); // метка активной карточки 
-
-  const errorMap = new Map([ [1006, "Не удалось найти город"] ]); // словарь возможных ошибок на русском
   
-  const statModifed = computed(() => {
-      return [
-        {
-          label: "Влажность",
-          stat: statistic.value.current.humidity + ' %', //вычисляемая часть
-        },
-        {
-          label: "Облачность",
-          stat: statistic.value.current.cloud + ' %',
-        },
-        {
-          label: "Ветер",
-          stat: (Math.round(parseInt(statistic.value.current.wind_kph) * (1000 / 3600) * 10) / 10) + ' м/с',
-        }
-      ]
-  });
+  const statistic = ref(); //реактивный объект статистики
+  const error = ref(); //реактивное состояние для случая, когда сапрос к WeatherAPI вернулся с ошибкой
+  let activeIndex = ref(0); // метка активной карточки  
 
-  const errorModified = computed(() => { // по коду ошибки определяет (как ключ), какое сообщение об ошибке будет отображено
-    return errorMap.get(error.value?.error?.code);
-  });
-
+  
   // функция получения названия города из компонента SelectCity
   async function getCity(city) { 
+    console.log(city);
     const params = new URLSearchParams({ //для корректного преобразования списка параметров запроса в URL 
       q: city,
       lang: "ru",
@@ -53,12 +28,8 @@
     } 
     error.value = null;
     statistic.value = await res.json(); // преобразование строки в объект (так как строка JSON очень большая нужно дождаться завершения преобразования)
-  }
-
-  function onCard(index) { // функция смены индекса активной карты
-    activeIndex.value = index;
-  }
-
+    console.log(statistic.value);
+  } 
   
 </script>
 
@@ -67,34 +38,13 @@
     <div class="left"></div>
 
     <div class="right">
-      <Error :error="errorModified"/>
-
-      <div v-if="statistic" class="stat-data">
-        <!-- Мультиплексирование информации о погоде, данные которой динамичкси (передаём как html-аргкмент) могут пересчитываться и всавляться в шаблон -->
-        <div class="stat-list">
-          <Statistic 
-            v-for="item in statModifed" 
-            v-bind="item" 
-            :key="item.label"
-          />
-        </div>
-        <div class="card-list">
-          <!-- Мультиплексирование карточек погоды -->
-          <DayCard 
-            v-for="( item, index ) in statistic.forecast.forecastday" 
-            :key="item.date" 
-            :weather-code="item.day.condition.code" 
-            :temperature="item.day.avgtemp_c" 
-            :day="new Date(item.date)" 
-            :is-active="activeIndex == index"
-            @click="onCard(index)"
-          />
-        </div>
-      </div>
-
-      <div class="select-style">
-        <SelectCity @select-city="getCity" />
-      </div>
+      <PanelRight 
+        :error
+        :statistic
+        :active-index="activeIndex" 
+        @select-index="(index) => activeIndex = index" 
+        @select-city="getCity"
+      />
     </div>
   </main>
 </template>
@@ -132,27 +82,4 @@
     border-radius: 30px;
 }
 
-  .card-list{
-    display: flex;
-    gap: 1px;
-  }
-
-  .select-style{
-    display: flex;
-    justify-content: center;
-    margin-bottom: 20px;
-  }
-
-  .stat-data{
-    display: flex;
-    flex-direction: column;
-    gap: 80px;
-    margin: 70px;
-  }
-
-  .stat-list{
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-  }
 </style>
---
+++ src/components/PanelRight.vue
@@ -0,0 +1,101 @@
+<script setup>
+    import { computed } from 'vue';
+    import DayCard from './DayCard.vue';
+    import Error from './Error.vue';
+    import SelectCity from './SelectCity.vue';
+    import Statistic from './Statistic.vue';
+    
+    const { error, statistic, activeIndex } = defineProps({
+        error: Object, 
+        statistic: Object,
+        activeIndex: Number,
+    });
+
+    const errorMap = new Map([ [1006, "Не удалось найти город"] ]); // словарь возможных ошибок на русском
+
+    const errorModified = computed(() => { // по коду ошибки определяет (как ключ), какое сообщение об ошибке будет отображено
+        return errorMap.get(error.error?.code);
+    });
+
+    const statData = computed(() => {
+        // if(!statistic){
+        //     return [];
+        // }
+    return [
+            {
+            label: "Влажность",
+            stat: statistic.current.humidity + ' %', //вычисляемая часть
+            },
+            {
+            label: "Облачность",
+            stat: statistic.current.cloud + ' %',
+            },
+            {
+            label: "Ветер",
+            stat: (Math.round(parseInt(statistic.current.wind_kph) * (1000 / 3600) * 10) / 10) + ' м/с',
+            }
+        ]
+    });
+
+    const emit = defineEmits(['select-index', 'select-city']);
+
+
+
+</script>
+
+<template>
+    <Error v-if="error" :error="errorModified"/>
+
+    <div v-if="statistic" class="stat-data">
+        <!-- Мультиплексирование информации о погоде, данные которой динамичкси (передаём как html-аргкмент) могут пересчитываться и всавляться в шаблон -->
+        <div class="stat-list">
+            <Statistic 
+                v-for="item in statData" 
+                v-bind="item" 
+                :key="item.label"
+            />
+        </div>
+        <div class="card-list">
+            <!-- Мультиплексирование карточек погоды -->
+            <DayCard 
+                v-for="( item, index ) in statistic.forecast.forecastday" 
+                :key="item.date" 
+                :weather-code="item.day.condition.code" 
+                :temperature="item.day.avgtemp_c" 
+                :day="new Date(item.date)" 
+                :is-active="activeIndex == index"
+                @click="() => emit('select-index', index)"
+            />
+        </div>
+    </div>
+
+    <div class="select-style">
+        <SelectCity @select-city="(city) => emit('select-city', city)" />
+    </div>
+</template>
+
+<style scoped>
+    .card-list{
+        display: flex;
+        gap: 10px;
+    }
+
+    .select-style{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20px;
+    }
+
+    .stat-data{
+        display: flex;
+        flex-direction: column;
+        gap: 80px;
+        margin: 70px;
+    }
+
+    .stat-list{
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+}
+</style>
